856fce36f230ac564215acd76675e7003f3d55fb
shaalx <shaalx@yahoo.com.sg>
2016-06-01 23:58:34 +0800

    test

--- c_test.go
+++ c_test.go
@@ -10,9 +10,9 @@ import (
 )
 
 var (
-	lis net.Listener
-	// rpc_tcp_server = "tcphub.t0.daoapp.io:61142"
-	rpc_tcp_server = "127.0.0.1:8800"
+	lis            net.Listener
+	rpc_tcp_server = "tcphub.t0.daoapp.io:61142"
+	// rpc_tcp_server = "127.0.0.1:8800"
 )
 
 func init() {
@@ -61,9 +61,9 @@ func TestJob(t *testing.T) {
 	// return
 	c := RPCClientWithCodec(rpc_tcp_server)
 	defer c.Close()
-	// in := Job{Name: "google", Target: "https://www.google.com/search?q=docker&oq=docker&aqs=chrome..69i57j69i60l4.1517j0j4&sourceid=chrome&ie=UTF-8"}
+	in := Job{Name: "google3", Target: "https://www.google.com/search?q=docker&oq=docker&aqs=chrome..69i57j69i60l4.1517j0j4&sourceid=chrome&ie=UTF-8"}
 	// in := Job{Name: "docker", Target: "https://www.google.com/aclk?sa=l&ai=Cls_gs3pMV8qDGcuq9gWdmIDoBprG9PcJgp_wutwC7MbsFAgAEAFgibPGhPQToAG8ppTsA8gBAaoEJk_QWc_UQjNHKe39e-t7guvDvFwTnmO55c8m1AmJHWa40wgOFtPpgAes2esTkAcBqAemvhvYBwE&sig=AOD64_1CJPrrkYMy7tE2O8DDMb1KMt8GuQ&clui=0&ved=0ahUKEwiagI7aq4LNAhXhnaYKHX5UDeQQ0QwIEg&adurl=https://circleci.com/integrations/docker/"}
-	in := Job{Name: "mdblog", Target: "http://mdblog.daoapp.io"}
+	// in := Job{Name: "mdblog", Target: "http://mdblog.daoapp.io"}
 	out := make([]byte, 10)
 	err := c.Call("RPC.Job", &in, &out)
 	goutils.CheckErr(err)
